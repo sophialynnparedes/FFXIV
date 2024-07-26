1036557013f794a4ebe2b0b8cdd7ad1a78fdd744
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-07-25 20:46:58 -0500

    set up firebase auth

---
+++ src/firebase.jsx
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, signInWithPopup, signOut, GoogleAuthProvider,} from "firebase/auth";
+import {getFirestore, getDoc, doc, setDoc} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "ffxiv-f220f.firebaseapp.com",
+  projectId: "ffxiv-f220f",
+  storageBucket: "ffxiv-f220f.appspot.com",
+  messagingSenderId: "1023743808342",
+  appId: "1:1023743808342:web:eb0c7424eea6696bd4bcf7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({prompt: "select_account",});
+
+export const login = async() => {
+    try{
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        await setDoc(
+            doc(db, "users", user.uid),
+            {
+                uid: user.uid,
+            },
+            {merge: true}
+        );
+    } catch (error){
+        alert(error.message);
+    }
+};
+
+export const logout = async() => {
+    try{
+        await signOut(auth);
+    } catch(error){
+        alert(error.message);
+    }
+}
