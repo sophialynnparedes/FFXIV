ccaf8c30471845ca654c56431ad747a285c6e8c4
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-07-26 07:57:26 -0500

    added background

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,7 @@
-
-export default function Row(props){
-    return (
-        <div className={"row " + props.align}>
-            {props.children}
-        </div>
-    )
+export default function Row(props) {
+  return (
+    <div className={"row " + props.align} id={props.id}>
+      {props.children}
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Row from "../components/Row";
 
 export default function Home() {
-  return <><Row align="center"><h1>Final Fantasy XIV TTRPG</h1></Row></>;
-}
+return <><Row align="center"><h1>Final Fantasy XIV TTRPG</h1></Row></>;
+}  
